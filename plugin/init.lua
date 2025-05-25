60f212f1bc11bf0a90e889b72fa5307b4ace6097
Steven Xu <stevenxxiu@gmail.com>
2025-05-26 03:54:58 +1000

    feat: work with *nvim-treesitter* `main` branch

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,7 +1,11 @@
-require('nvim-treesitter.parsers').get_parser_configs().mail = {
-  install_info = {
-    url = 'https://github.com/stevenxxiu/tree-sitter-mail',
-    files = { 'src/parser.c' },
-  },
-  filetype = 'mail',
-}
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'TSUpdate',
+  callback = function()
+    require('nvim-treesitter.parsers').mail = {
+      install_info = {
+        path = vim.fs.joinpath(vim.fn.stdpath('data'), 'lazy/tree-sitter-mail'),
+      },
+      tier = 3,
+    }
+  end,
+})
